31184299e7f630d2e40ca1abd8ddfd13c039f437
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-17 16:38:34 -0500

    Refactor

--- src/truffle-integration/projectFsWatcher.js
+++ src/truffle-integration/projectFsWatcher.js
@@ -37,7 +37,7 @@ class ProjectFsWatcher extends EventEmitter {
     this.configWatcher = fs.watch(
       this.project.configFile,
       { encoding: "utf8" },
-      (eventType, filename) => {
+      () => {
         // the config file was either removed or changed, we may want to reload it
 
         this.stopWatchingParentDirectory();

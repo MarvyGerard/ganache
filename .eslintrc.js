f80f0303bbad4f6932262aa917ce044b2b13ab63
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-17 16:22:43 -0500

    Use eslint-plugin-react recommended Disable prop-types for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
-  extends: ["prettier", "eslint:recommended"],
-  plugins: ["prettier"],
+  extends: ["prettier", "eslint:recommended", "plugin:react/recommended"],
+  plugins: ["prettier", "react"],
   parser: "babel-eslint",
   rules: {
     "prettier/prettier": [1, { trailingComma: "all" }],
-    "no-console": "off"
+    "no-console": "off",
+    "react/prop-types": 0
   },
   env: {
     es6: true,
     browser: true,
     node: true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 };
